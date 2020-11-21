eb28552a9e0a13981be4b66690e68aeb8edb49df
Justin Pricer <justin.pricer@gmail.com>
2020-11-21 10:32:32 -0600

    add routes

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var notes = require("../db/db.json");
+const { v4: uuidv4 } = require('uuid');
+let path = require("path");
+const { json } = require("body-parser");
+
+module.exports = function(app) {
+  
+  app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    let newNote = {
+      id : uuidv4(),
+      title : req.body.title,
+      text: req.body.text
+    }
+    notes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function(err){
+      if (err) throw err;
+      return res.json(newNote);
+    })
+    
+    
+  });
+
+
+app.delete("/api/notes/:id", function (req, res) {
+  let noteid = req.params.id;
+
+  for (let i = 0; i < notes.length; i++) {
+    if (noteid === notes[i].id) {
+      notes.splice(i, 1);
+      fs.writeFile("./db/db.json", JSON.stringify(notes), function(err){
+        if (err) throw err;
+        return res.json(notes);
+      })  
+    }
+  }
+
+})
+};
--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 3003;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-
-require("./routes/apiRoutes")(app);
-require("./routes/HTMLroutes")(app);
 
 
 app.listen(PORT, function() {
